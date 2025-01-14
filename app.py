4fc0cf12f1cbabeee3677092db8b344d84f36a69
Yunusa M. Namadi <98776848+ynamadi@users.noreply.github.com>
2025-01-14 17:19:45 -0500

    Initial files
    Hello World

---
+++ app.py
@@ -0,0 +1,18 @@
+# from flask import Flask, render_template
+# app = Flask(__name__)
+#
+#
+# @app.route('/')
+# def hello():
+#   return render_template('index.html')
+#
+#
+#
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html', message='Hello, World!')
